219221004bb8c13d039523f51e3fef2525811aac
Francesca Dreith <francescadreith@Francescas-MacBook-Air.local>
2020-04-30 17:49:08 -0700

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,12 +1,18 @@
 export function manageFriends(state, action) {
+
   switch (action.type) {
     case 'ADD_FRIEND':
-      console.log(action.friend)
-      console.log(state)
-
-      return { friends: [Object.assign({}, state.friends, action.friend)] }
+      return ({ ...state, friends: [...state.friends, action.friend] })
     case 'REMOVE_FRIEND':
-      return {}
+      const removalIndex = state.friends.findIndex(friend => friend.id === action.id)
+      return (
+        {
+          ...state,
+          friends: [
+            ...state.friends.slice(0, removalIndex),
+            ...state.friends.slice(removalIndex + 1)]
+        }
+      )
     default:
       return state
   }

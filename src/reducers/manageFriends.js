6caba70aa47b30fdadaaeb8a98f1eaf736622826
Francesca Dreith <francescadreith@Francescas-MacBook-Air.local>
2020-04-30 17:44:44 -0700

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,13 @@
-export function manageFriends(state, action){
+export function manageFriends(state, action) {
+  switch (action.type) {
+    case 'ADD_FRIEND':
+      console.log(action.friend)
+      console.log(state)
+
+      return { friends: [Object.assign({}, state.friends, action.friend)] }
+    case 'REMOVE_FRIEND':
+      return {}
+    default:
+      return state
+  }
 }

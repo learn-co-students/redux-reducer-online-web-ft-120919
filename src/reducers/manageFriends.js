1b97f3a27acf12e83e71fc52f73024a5c464c18d
rick <github email address>
2020-05-04 16:54:47 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,19 @@
-export function manageFriends(state, action){
-}
+export const manageFriends = (state, action) => {
+  if (action.type === "ADD_FRIEND") {
+    state.friends.push({
+      name: action.friend.name,
+      hometown: action.friend.hometown,
+      id: action.friend.id,
+    });
+    return state;
+  } else if (action.type === "REMOVE_FRIEND") {
+    let result = { friends: [] };
+    state.friends.forEach((friend, index) => {
+      friend.id === action.id ? result.friends.push(friend) : null;
+    });
+    let difference = state.friends.filter((x) => !result.friends.includes(x));
+    return { friends: difference };
+  } else {
+    return state;
+  }
+};
--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,6 @@
-export function managePresents(state, action){
+export const managePresents =  (state, action) => {
+  let increased = { numberOfPresents: state.numberOfPresents + 1 };
+  let [def, increaseOrNot] = [{ numberOfPresents: 0 }, action.type === "INCREASE"]
+  return increaseOrNot ? increased : def;
 }
+ 

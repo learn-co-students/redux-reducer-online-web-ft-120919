f75192197ff327b1a5ad0bb476c4c08d99ee8cf4
Noah Scala <nwscala@gmail.com>
2020-05-04 11:21:17 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,12 @@
-export function manageFriends(state, action){
+export function manageFriends(state = { friends: []}, action){
+    switch(action.type) {
+        case "ADD_FRIEND":
+            return { ...state, friends: [...state.friends, action.friend] }
+        case "REMOVE_FRIEND":
+            const thisFriend = state.friends.find(friend => friend.id === action.id)
+            const thisFriendIndex = state.friends.indexOf(thisFriend)
+            return { ...state, friends: [...state.friends.slice(0, thisFriendIndex), ...state.friends.slice(thisFriendIndex + 1)] }
+        default:
+            return state
+    }
 }

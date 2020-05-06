18613001fca385d061304767296e68bbc516888f
asengelhart <asengelhart@yahoo.com>
2020-05-06 16:59:58 -0600

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,11 @@
-export function manageFriends(state, action){
+export function manageFriends(state={friends: []}, action){
+  switch(action.type) {
+    case "ADD_FRIEND":
+      return {friends: [...state.friends, action.friend]};
+    case "REMOVE_FRIEND":
+      const friendsArr = state.friends.filter(friend => friend.id !== action.id);
+      return {friends: friendsArr};
+    default:
+      return state;
+  }
 }
